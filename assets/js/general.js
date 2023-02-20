d3c13e148a2fac08b011ad77d4ffdac36b7f2b9b
Camila Arias <ariasruizcamilaa@gmail.com>
2023-02-20 11:03:47 -0500

    creación páginas de errores

--- assets/js/general.js
+++ assets/js/general.js
@@ -1,3 +1,4 @@
 document.write('<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>');
 document.write('<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"></script>');
 document.write('<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"></script>');
+document.write('<script src="https://kit.fontawesome.com/yourcode.js"></script>')
